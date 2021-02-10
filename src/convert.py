e8c485ff4dcc9d7ad3bb43f9ad5d9a32d77d36e8
Marco Cenerelli <marco.cenerelli@genevalab.com>
2021-02-10 14:07:17 +0100

    Converter script

--- src/convert.py
+++ src/convert.py
@@ -1,7 +1,37 @@
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 
-new_model= tf.keras.models.load_model(filepath="C:\\Users\\PLundberg\\Desktop\\damn\\model.h5")
+model = Sequential()
 
-converter = tf.lite.TFLiteConverter.from_keras_model(new_model)
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+#
+model.load_weights('model.h5')
+
+# model.save('model_with_structure.h5')
+#
+# model = tf.keras.models.load_model(filepath="model_with_structure.h5")
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 open("tf_lite_model.tflite", "wb").write(tflite_model)
